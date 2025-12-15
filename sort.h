fc05fad3d9b93beb171a3a2e863c4e2100574ffd
rabea14180-max <rabea14180@gmail.com>
2025-12-15 15:18:01 +0000

    add a function Bubble sort

--- sort.h
+++ sort.h
@@ -15,6 +15,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 
