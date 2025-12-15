7116b1fd0b6c136e6e6fc8d073c1d00e740c709e
rabea14180-max <rabea14180@gmail.com>
2025-12-15 13:26:03 +0000

    add a functionBubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
+#include "sort.h"
 /**
  * print_array - Prints an array of integers
  * @array: Pointer to the array
@@ -32,11 +33,11 @@ printf("\n");
  */
 void bubble_sort(int *array, size_t size)
 {
-if (!array || size < 2)
-return;
 size_t i, j;
 int temp;
 int swapped;
+if (!array || size < 2)
+return;
 for (i = 0; i < size - 1; i++)
 {
 swapped = 0;
@@ -44,11 +45,14 @@ for (j = 0; j < size - i - 1; j++)
 {
 if (array[j] > array[j + 1])
 {
-temp = array[j];
+temp = array[j]
 array[j] = array[j + 1];
 array[j + 1] = temp;
+swapped = 1;
+print_array(array, size);
 }
 }
 if (!swapped)
 break;
+}
 }
