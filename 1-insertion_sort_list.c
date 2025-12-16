fa6a3fdffef3ede34d82bfa937479c748579d282
rabea14180-max <rabea14180@gmail.com>
2025-12-16 11:51:55 +0000

    add  a function Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,39 +2,42 @@
 #include <stdlib.h>
 #include "sort.h"
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- *                       order using the Insertion sort algorithm.
- * @list: Pointer to the doubly linked list.
- *
- * Description: The function sorts a doubly linked list in ascending order
- * using the Insertion sort algorithm. It swaps nodes, not their values.
- * After each swap, the list is printed.
+ * swap_nodes - swaps two adjacent nodes in a doubly linked list
+ * @list: pointer to head of list
+ * @left: first node
+ * @right: second node
+ */
+static void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
+{
+if (left->prev)
+left->prev->next = right;
+else
+*list = right;
+if (right->next)
+right->next->prev = left;
+left->next = right->next;
+right->prev = left->prev;
+right->next = left;
+left->prev = right;
+}
+/**
+ * insertion_sort_list - sorts a doubly linked list using insertion sort
+ * @list: pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *current, *temp, *insert_pos;
-if (list == NULL || *list == NULL || (*list)->next == NULL)
+listint_t *current, *tmp;
+if (!list || !*list || !(*list)->next)
 return;
 current = (*list)->next;
-while (current != NULL)
+while (current)
 {
-insert_pos = current;
-while (insert_pos->prev != NULL && insert_pos->n < insert_pos->prev->n)
-{
-temp = insert_pos->prev;
-if (temp->prev != NULL)
-temp->prev->next = insert_pos;
-else
-*list = insert_pos;
-temp->next = insert_pos->next;
-if (insert_pos->next != NULL)
-insert_pos->next->prev = temp;
-insert_pos->prev = temp->prev;
-temp->prev = insert_pos;
-insert_pos->next = temp;
-print_list(*list);
-insert_pos = insert_pos->prev;
-}
+tmp = current;
 current = current->next;
+while (tmp->prev && tmp->n < tmp->prev->n)
+{
+swap_nodes(list, tmp->prev, tmp);
+print_list(*list);
+}
 }
 }
