f3c0204914247ce38a1a0191125c51f0ed82c7e4
rabea14180-max <rabea14180@gmail.com>
2025-12-16 13:28:02 +0000

    add  a function Quick sort

---
+++ main_8.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "sort.h"
+#include <stdio.h>
+/**
+ * main - Entry point to test quick_sort
+ * Return: 0
+ */
+int main(void)
+{
+int array[] = {98, 98, 98, 98, 98, 98, 98, 98, 98, 98,
+98, 98, 98, 98, 98, 98, 98, 98, 98, 98};
+size_t n = sizeof(array) / sizeof(array[0]);
+print_array(array, n);
+quick_sort(array, n);
+print_array(array, n);
+return (0);
+}
