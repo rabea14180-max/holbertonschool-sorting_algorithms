b6fde4e05517aaeb78d38d8d2b94e79f0c3ff378
rabea14180-max <rabea14180@gmail.com>
2025-12-16 12:52:25 +0000

    add  a function Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
+#include "sort.h"
 /**
- * swap_ints - swaps two integers in an array
+ * swap_ints - swaps two integers
  * @a: first integer
  * @b: second integer
  */
@@ -13,53 +14,53 @@ int tmp = *a;
 *b = tmp;
 }
 /**
- * lomuto_partition - partitions the array using Lomuto scheme
+ * lomuto_partition - Lomuto partition scheme
  * @array: array of integers
  * @low: starting index
  * @high: ending index (pivot)
- * @size: size of the array (for printing)
+ * @size: size of array
  *
- * Return: index of the pivot after partition
+ * Return: pivot final index
  */
 static int lomuto_partition(int *array, int low, int high, size_t size)
 {
 int pivot = array[high];
-int i = low - 1;
+int i = low;
 int j;
 for (j = low; j < high; j++)
 {
 if (array[j] < pivot)
 {
-i++;
 if (i != j)
 {
 swap_ints(&array[i], &array[j]);
 print_array(array, size);
 }
+i++;
 }
 }
-if (i + 1 != high)
+if (i != high)
 {
-swap_ints(&array[i + 1], &array[high]);
+swap_ints(&array[i], &array[high]);
 print_array(array, size);
 }
-return (i + 1);
+return (i);
 }
 /**
- * quick_sort_recursive - recursive Quick sort helper
+ * quick_sort_recursive - recursive quick sort
  * @array: array of integers
  * @low: starting index
  * @high: ending index
- * @size: size of the array
+ * @size: size of array
  */
 static void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
-int pi;
+int p;
 if (low < high)
 {
-pi = lomuto_partition(array, low, high, size);
-quick_sort_recursive(array, low, pi - 1, size);
-quick_sort_recursive(array, pi + 1, high, size);
+p = lomuto_partition(array, low, high, size);
+quick_sort_recursive(array, low, p - 1, size);
+quick_sort_recursive(array, p + 1, high, size);
 }
 }
 /**
@@ -73,4 +74,3 @@ if (!array || size < 2)
 return;
 quick_sort_recursive(array, 0, (int)size - 1, size);
 }
-
